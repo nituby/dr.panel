c8f8f2e715811b53cdbd60c438b7d5e3f7e42695
nitu.dhaka <nitu.dhaka@buzzyears.com>
2017-09-16 16:28:39 +0530

    added google column chart

--- src/app/graph-info/graph-draw.component.ts
+++ src/app/graph-info/graph-draw.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
+import { GoogleChartComponent } from './google-chart.component';
 
 @Component({
   selector: 'app-graph-draw',
   templateUrl: './graph-draw.component.html',
   // styleUrls: ['./graph-draw.component.scss']
 })
-export class GraphDrawComponent {
-  title = 'app';
+export class GraphDrawComponent extends GoogleChartComponent{
+    private options;
+  private data;
+  private chart;
+  // constructor(){
+  //   console.log("Here is EvolutionComponent")
+  // }
+
+  drawGraph(){
+    console.log("DrawGraph Evolution...");  
+    this.data = this.createDataTable([
+      ['Evolution', 'Imports', 'Exports'],
+      ['A', 8695000, 6422800],
+      ['B', 3792000, 3694000],
+      ['C', 8175000, 800800]
+    ]);
+
+    this.options = {
+      title: 'Evolution, 2014',
+      chartArea: {width: '50%'},
+      hAxis: {
+        title: 'Value in USD',
+        minValue: 0
+      },
+      vAxis: {
+        title: 'Members'
+      }
+    };
+
+    this.chart = this.createBarChart(document.getElementById('chart_divEvolution'));
+    this.chart.draw(this.data, this.options);
+  }
 }
